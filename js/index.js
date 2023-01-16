9d3b88a1e85db96d39dbf461af02bc98fd7d3af4
Sarilus-Blaise <sarilusblondywadley@gmail.com>
2023-01-16 18:03:58 -0500

    behaviors have been added

--- js/index.js
+++ js/index.js
@@ -1,28 +1,16 @@
-// !! IMPORTANT README:
+const scrollLinks = document.querySelectorAll(".scroll-link");
+const header = document.getElementById("header");
+const headerHeight = header.getBoundingClientRect().height;
 
-// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
+scrollLinks.forEach((link) => {
+	link.addEventListener("click", (event) => {
+		event.preventDefault();
+		const id = event.currentTarget.getAttribute("href").slice(1);
+		const element = document.getElementById(id);
+		const position = element.offsetTop - headerHeight;
 
-/***********
-INSTRUCTIONS:
-  - Select the project you would 
-    like to complete from the dropdown 
-    menu.
-  - Click the "RUN TESTS" button to
-    run the tests against the blank 
-    pen.
-  - Click the "TESTS" button to see 
-    the individual test cases. 
-    (should all be failing at first)
-  - Start coding! As you fulfill each
-    test case, you will see them go   
-    from red to green.
-  - As you start to build out your 
-    project, when tests are failing, 
-    you should get helpful errors 
-    along the way!
-    ************/
-
-// PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
-
-// Once you have read the above messages, you can delete all comments. 
-"use strict";
+		window.scrollTo({
+			top: position,
+		});
+	});
+});
